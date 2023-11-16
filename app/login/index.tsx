cfe50654d6b5bbb075938496ca138a3fcf8d3da5
CoffeESIME <heromfabian@gmail.com>
2023-11-16 11:56:23 -0600

    solved issue with react hook form

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,8 +1,8 @@
 import { Text } from 'react-native-paper';
 import { View, StyleSheet } from 'react-native';
-import { useRouter } from 'expo-router';
-import { CardQ } from '../../src/components/CardQ/CardQ';
-import { ButtonQ } from '../../src/components/ButtonQ/ButtonQ';
+// import { useRouter } from 'expo-router';
+// import { CardQ } from '../../src/components/CardQ/CardQ';
+// import { ButtonQ } from '../../src/components/ButtonQ/ButtonQ';
 import { ControlledInputQ } from '../../src/components/ControlledInputQ/ControlledInputQ';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
